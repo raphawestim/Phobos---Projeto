beae40426a0c940e0a900eda5db015abec5565cb
Raphael Westim Leite do Carmo <rwestim@uolinc.com>
2022-05-10 10:27:42 -0300

    nova alteração

--- js/index.js
+++ js/index.js
@@ -11,3 +11,26 @@ function paginaAnimal(){
     window.location = "../data/CatalogoAnimal.html"
  }
 
+ const numeroVisu = document.querySelector('.cartao_info--visualiza');
+ const btnVisu = document.querySelector("[data-visualizacao]");
+
+let visualizacoes = 0;
+
+function addVisual(){
+    
+    visualizacoes++;
+    numeroVisu.innerText = `${visualizacoes} Visualizações`;
+}
+
+btnVisu.addEventListener("click", addVisual);
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    //console.log("DOM completamente carregado e analisado");
+    $(".js-video-button").modalVideo({
+        youtube:{
+            controls:0,
+            nocookie: true
+        },
+  url: "https://developer.a-blogcms.jp/themes/developer/movies/280.mp4"
+    });
+  });
